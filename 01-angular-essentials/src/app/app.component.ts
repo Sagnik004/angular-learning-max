d327af5ebb1d1453b6cd6fdea56673801258238e
Sagnik004 <sagnikchakraborty910@yahoo.com>
2024-06-28 22:46:29 +0530

    init commit

---
+++ 01-angular-essentials/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+import { HeaderComponent } from './component/header/header.component';
+import { UserComponent } from './component/user/user.component';
+import { DUMMY_USERS } from './dummy-users';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [HeaderComponent, UserComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  users = DUMMY_USERS;
+
+  onSelectUser(id: string) {
+    console.log('Selected user with id ' + id);
+  }
+}
